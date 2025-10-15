3f8b88aa97fadba4bba8670294fdecdae6bf3593
Jacob Kuzmits <kuzmits@gmail.com>
2025-10-14 23:07:54 -0400

    feat: integrate get_files_info tool into agent tools

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
+from functions.get_files_info import schema_get_files_info
 
 def main():
+    # initialization
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    # verify a prompt was given as a command line argument
     if len(sys.argv) == 1:
         sys.exit(1)
     prompt = sys.argv[1]
@@ -16,14 +20,39 @@ def main():
     if "--verbose" in sys.argv:
         verbose_mode = True
     
-    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+    # configure LLM
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
     
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt))
-    print(response.text)
+    response = client.models.generate_content(model="gemini-2.0-flash-001", 
+                                              contents=messages, 
+                                              config=types.GenerateContentConfig(system_instruction=system_prompt, tools=[available_functions]),
+                                              )
+
+    if response.text:
+        print(response.text)
+
+    if len(response.function_calls) > 0:
+        for call in response.function_calls:
+            print(f"Calling function: {call.name}({call.args})")
+
     if verbose_mode:
         print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
