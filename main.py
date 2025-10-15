66625e1c1737c5b95a1ee3f768a7a8a6a374e306
Jacob Kuzmits <kuzmits@gmail.com>
2025-10-14 20:47:05 -0400

    feat; add calculator tool

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import sys
+
 def main():
-    print("Hello from bootdev-ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    if len(sys.argv) == 1:
+        sys.exit(1)
+    prompt = sys.argv[1]
+
+    verbose_mode = False
+    if "--verbose" in sys.argv:
+        verbose_mode = True
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    print(response.text)
+    if verbose_mode:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
     main()
+
